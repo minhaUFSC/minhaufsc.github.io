e9077160d02402d7fd936978270b2f5a302cf9f1
CrazyHatish <phg1608@gmail.com>
2018-02-09 15:26:57 +0100

    Update to follow UFSC design guidelines

--- script.js
+++ script.js
@@ -29,11 +29,6 @@ function canv() {
   img.onload = function() {
     ctx.drawImage(img, 0, 0);
     ctx.fillStyle = "white";
-    ctx.font = "bold 128px Verdana, Geneva, sans-serif";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "top";
-    ctx.fillText("UFSC", c.width/2, c.height/4 + 20)
-
     ctx.font = font_size +"px Verdana, Geneva, sans-serif";
     ctx.textAlign = "right";
     ctx.textBaseline = "middle";
@@ -48,60 +43,3 @@ function down() {
   $("#download").attr("href", (document.getElementById("canvas").toDataURL()))
 
 }
-
-
-// Set caret position easily in jQuery
-// Written by and Copyright of Luke Morton, 2011
-// Licensed under MIT
-(function ($) {
-    // Behind the scenes method deals with browser
-    // idiosyncrasies and such
-    $.caretTo = function (el, index) {
-        if (el.createTextRange) {
-            var range = el.createTextRange();
-            range.move("character", index);
-            range.select();
-        } else if (el.selectionStart != null) {
-            el.focus();
-            el.setSelectionRange(index, index);
-        }
-    };
-
-    // The following methods are queued under fx for more
-    // flexibility when combining with $.fn.delay() and
-    // jQuery effects.
-
-    // Set caret to a particular index
-    $.fn.caretTo = function (index, offset) {
-        return this.queue(function (next) {
-            if (isNaN(index)) {
-                var i = $(this).val().indexOf(index);
-
-                if (offset === true) {
-                    i += index.length;
-                } else if (offset) {
-                    i += offset;
-                }
-
-                $.caretTo(this, i);
-            } else {
-                $.caretTo(this, index);
-            }
-
-            next();
-        });
-    };
-
-    // Set caret to beginning of an element
-    $.fn.caretToStart = function () {
-        return this.caretTo(0);
-    };
-
-    // Set caret to the end of an element
-    $.fn.caretToEnd = function () {
-        return this.queue(function (next) {
-            $.caretTo(this, $(this).val().length);
-            next();
-        });
-    };
-}(jQuery));
